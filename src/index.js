9b4e845ca39c977f594d1a76afedfff2b5dcf4d1
RajeebKumarpanda <rajeeb009@gmail.com>
2025-03-23 20:13:58 +0530

    cronjs added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 //middlewares
+job.start();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
